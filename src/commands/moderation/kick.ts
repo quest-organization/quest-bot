60148d63e2869a92a179c5e648681ce7dcd64021
lunarash0 <eamonhwatson@icloud.com>
2026-04-18 05:22:40 +0200

    feat: kick command

---
+++ src/commands/moderation/kick.ts
@@ -0,0 +1,64 @@
+import type { Command } from '../index.js';
+import { ButtonBuilder, ButtonStyle, ActionRowBuilder, ApplicationCommandOptionType, PermissionsBitField, GuildMember } from 'discord.js';
+import { emojis } from '#utils/emoji.js';
+
+export default {
+	data: {
+		name: 'kick',
+		description: 'Kick someone from the discord serve.',
+        options: [
+            {
+                type: ApplicationCommandOptionType.User,
+                name: "target",
+                description: "Select a user to kick",
+                required: true
+            },
+            {
+                type: ApplicationCommandOptionType.String,
+                name: "reason",
+                description: "Provide a reason for their kick"
+            }
+        ]
+	},
+	async execute(interaction) {
+        const member = interaction.member as GuildMember;
+        
+        if (!member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+            return
+        }
+        
+		const target = interaction.options.getUser('target', true);
+		const reason = interaction.options.getString('reason') ?? 'No reason provided';
+
+		const confirm = new ButtonBuilder()
+            .setCustomId('confirm')
+			.setLabel('Confirm Ban')
+			.setStyle(ButtonStyle.Danger)
+        
+        const cancel = new ButtonBuilder()
+            .setCustomId('cancel')
+			.setLabel('Cancel')
+			.setStyle(ButtonStyle.Secondary)
+        
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(cancel, confirm);
+        const response = await interaction.reply({
+			content: `${emojis.rightArrow1} Are you sure you want to kick ${target} for reason: ${reason}?`,
+			components: [row],
+            withResponse: true,
+		});
+        
+        const collectorFilter = (i: { user: { id: string; }; }) => i.user.id === interaction.user.id;
+        try {
+            const confirmation = await response.resource!.message!.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+            
+            if (confirmation.customId === 'confirm') {
+                await interaction.guild!.members.kick(target);
+                await confirmation.update({ content: `${emojis.rightArrow2} ${target.username} has been kicked for reason: ${reason}\nYou must have had a real ick towards that person.`, components: [] });
+            } else if (confirmation.customId === 'cancel') {
+                await confirmation.update({ content: `${emojis.rightArrow2} Cancelled.`, components: [] });
+            }
+        } catch {
+            await interaction.editReply({ content: `${emojis.rightArrow2} No response within a minute or errored.`, components: [] });
+        }
+	},
+} satisfies Command;
